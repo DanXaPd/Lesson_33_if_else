eca18fc4a53664a15fcb3b48ea430ecb61b2f7f3
DanXaPd <92036667+DanXaPd@users.noreply.github.com>
2022-01-28 14:28:52 +0200

    Exercises if_else

---
+++ main.js
@@ -0,0 +1,57 @@
+// 3 конструкции if JS
+
+// I if
+// if (условие) {
+//     инструкция
+// }
+
+// II if else
+
+// if (условие) {
+//     инструкция1
+// } else {
+//     инструкция2 
+// }
+
+// III каскадное ветвление if else-if else
+
+// if (условие1) {
+//     инструкцию1
+// } else if (условие2) {
+//     инструкция2
+// } else if (условие3) {
+//     инструкция3
+// }
+// ...
+// else {
+//     инструкциюN
+// }
+
+
+/*let birth_of_year = prompt("Введите пожалуйста ваш возраст: ")
+if (birth_of_year == 25) {
+    alert("Ты уже взрослый")
+    alert(++birth_of_year)
+} else if(birth_of_year > 0 && birth_of_year < 25){
+    alert("Можно идти на ночевку на солнечный")
+} else if(birth_of_year >= 100){
+    alert("Так долго не живут!!!")
+} else{
+    alert("ТЫ КТО!!!!!!")
+}*/
+
+
+// Упращенная конструкция if
+
+//0let value = условие ? значение_1 : значение_2
+/*let age = +prompt("AGE???")
+let a = (age < 3) ? alert("Ты еще малыш") : alert("Ты взрослый дядя")*/
+
+
+/*let birth_of_year = prompt("Введите пожалуйста ваш возраст: ")
+let ageUser = (birth_of_year == 25) ? alert("Ты уже взрослый") : (birth_of_year > 0 && birth_of_year < 25) ? alert("Ты еще маленький") : alert("ТЫ КТО!!!!!")*/
+
+
+
+
+
